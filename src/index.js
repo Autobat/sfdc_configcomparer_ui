4f8395776939c560053c03626b68da276a969263
Chris Mail <chris@autobat.me>
2017-11-10 09:23:57 +1100

    Initial move to components and SLDS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React 	from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import App 		from './App';
+import Header 	from './components/header.js';
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Header />, document.getElementById('header'));
+
 registerServiceWorker();
